1d794241c496e9a16bbd256d048f11ece7d4a358
Facundito <facundito@Facunditos-MacBook-Pro.local>
2023-04-22 13:19:16 -0300

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,51 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, [], init)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+    
+
+    const handleNewTodo = (todo) => {
+        const action ={
+            type: 'Add Todo',
+            payload: todo,
+        }
+
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: 'Remove Todo',
+            payload: id,
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: 'Toggle Todo',
+            payload: id,
+        })
+    }
+
+  return (
+    {
+        todos,
+        handleDeleteTodo,
+        handleNewTodo,
+        handleToggleTodo,
+        pendingTodosCount: todos.filter(todo=>!todo.done).length,
+        todosCount: todos.length,
+    }
+  )
+}
